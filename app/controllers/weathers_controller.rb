b232510604b5e4cc6819b53e82373d603f0c2658
Diego <diegof.df58@gmail.com>
2021-07-01 01:07:07 -0400

    add wind, clouds methods and validations

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -11,30 +11,84 @@ class WeathersController < ApplicationController
     end 
 
     def create_temperature
-        @weather = Weather.new(weather_params)
-       
-       
+        @weather = Weather.new(weather_temp_param)
+
             if @weather.save
 
                 if  @weather.degrees < 0
                     msg = {:description => "#{@weather.degrees.to_s + I18n.t('temperature.below_zero')}"}
                     render :json => msg, status: 200
-                end
-
-                if @weather.degrees >= 0
+                elsif @weather.degrees >= 0
                     msg = {:description => "#{@weather.degrees.to_s + I18n.t('temperature.above_zero')}"}
                     render :json => msg, status: 200
-                end
-              
-            else
-                
-            end
-     
 
+                end 
+            else
+                msg = {:description => "Can´t save wheather"}
+                    render :json => msg, status: 404
+            end
     end
+    
+    def create_wind
+        @weather = Weather.new(weather_wind_param)
+
+            if @weather.save
+                if  @weather.wind_speed < 3
+                    msg = {:description => I18n.t('wind.none')}
+                    render :json => msg, status: 200
+                elsif @weather.wind_speed < 8
+                    msg = {:description => I18n.t('wind.weak')}
+                    render :json => msg, status: 200
+                elsif @weather.wind_speed < 20
+                    msg = {:description => I18n.t('wind.medium')}
+                    render :json => msg, status: 200
+                elsif @weather.wind_speed >= 20
+                    msg = {:description => I18n.t('wind.strong')}
+                    render :json => msg, status: 200
+                end
+            else
+                msg = {:description => "Can´t save wind speed"}
+                    render :json => msg, status: 404
+            end
+    end
+
+    def create_cloud
+        @weather = Weather.new(weather_cloud_param)
+
+            if @weather.save
+                if @weather.clouds <= 10
+                    msg = {:description => I18n.t('clouds.none')}
+                    render :json => msg, status: 200
+                elsif @weather.clouds <= 70
+                    msg = {:description => I18n.t('clouds.partial')}
+                    render :json => msg, status: 200
+                elsif @weather.clouds > 70
+                    msg = {:description => I18n.t('clouds.full')}
+                    render :json => msg, status: 200
+                end
+            else
+                msg = {:description => "Can´t save cloud"}
+                    render :json => msg, status: 404
+            end
+    end
+
+
+    rescue_from ActionController::ParameterMissing do |exception|     
+        msg = {:error => "Parameter missing"}
+        render :json => msg, status: 422
+    end
+    
     private
-        def weather_params
-            params.require(:weather).permit(:degrees, :wind_speed, :clouds)
+        def weather_temp_param
+            params.require(:weather).permit(:degrees)
+        end
+
+        def weather_wind_param
+            params.require(:weather).permit(:wind_speed)
+        end
+
+        def weather_cloud_param
+            params.require(:weather).permit(:clouds)
         end
    
     
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
     collection do
       post '/temperature', to: "weathers#create_temperature"
     end
+    collection do
+      post '/wind', to: "weathers#create_wind"
+    end
+    collection do
+      post '/clouds', to: "weathers#create_cloud"
+    end
   
   end
 end
