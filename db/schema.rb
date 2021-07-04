4045bbef530ba9018caf307997a64de693c76c62
Diego <diegof.df58@gmail.com>
2021-07-03 20:50:41 -0400

    Add JWT Auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_213003) do
+ActiveRecord::Schema.define(version: 2021_07_04_004332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "weathers", force: :cascade do |t|
     t.integer "degrees"
